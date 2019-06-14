250561d19a8b7cb3140482163e9187377b4cb044
Alex Koval <al3xander.koval@gmail.com>
2019-06-14 19:06:00 +0300

    Check DB connection every 30 seconds

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,15 @@ Hanami.configure do
 
     migrations 'db/migrations'
     schema 'db/schema.sql'
+
+    # Use connection_validator sequel plugin for setup how often we need to check are connections
+    # valid in connection pool or not. If connections invalid (DB instance was restarted) we will
+    # reopen all connections again. In our case we will do it every 30 second or
+    # `ENV['DATABASE_CONNECTION_VALIDATION_TIMEOUT']` seconds.
+    gateway do |g|
+      g.connection.extension(:connection_validator)
+      g.connection.pool.connection_validation_timeout = ENV['DATABASE_CONNECTION_VALIDATION_TIMEOUT'] || 30 # seconds
+    end
   end
   logger level: :debug
   mailer do

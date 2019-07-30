3dfd5af7fd28f8a789c7d9aa821d877a87d85f40
Alex Koval <al3xander.koval@gmail.com>
2019-07-30 17:47:57 +0300

    delete event field if all event fields are empty

--- apps/web/forms/talk_form.rb
+++ apps/web/forms/talk_form.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require 'hanami/utils/blank'
+
 module Web
   module Forms
     class TalkForm
       SpeakerSchema = Dry::Validation.Form do
+        optional(:id).filled(:int?)
         required(:first_name).filled(:str?)
         required(:last_name).filled(:str?)
       end
 
       EventSchema = Dry::Validation.Form do
+        optional(:id)
         required(:name).filled(:str?)
         required(:city).filled(:str?)
         required(:started_at).filled(:str?)
@@ -20,11 +24,13 @@ module Web
         required(:description).filled(:str?)
         required(:talked_at).filled(:date_time?)
         required(:link).filled(:str?)
-        required(:speaker).schema(SpeakerSchema)
+        required(:speakers).each(SpeakerSchema)
         optional(:event).schema(EventSchema)
       end
 
       def call(params)
+        # not sure this is appropriate place
+        params.delete(:event) if params[:event]&.values&.all? { |value| Hanami::Utils::Blank.blank?(value) }
         TalkSchema.call(params)
       end
     end

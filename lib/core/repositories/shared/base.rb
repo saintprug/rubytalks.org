6d58530f5d0c1792c31c98c9b5d4ae43312c59cd
Alex Koval <al3xander.koval@gmail.com>
2019-07-30 21:46:49 +0300

    use one line

--- lib/core/repositories/shared/base.rb
+++ lib/core/repositories/shared/base.rb
@@ -4,12 +4,10 @@ module Base
   private
 
   def order_by_created_at(relation)
-    relation
-      .order { created_at.desc }
+    relation.order { created_at.desc }
   end
 
   def with_relations(relation, *relation_names)
-    relation
-      .combine(*relation_names)
+    relation.combine(*relation_names)
   end
 end
